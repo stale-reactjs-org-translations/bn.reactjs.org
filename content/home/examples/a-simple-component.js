70650762a90caad5c44ef06fde11f3c13b1d9a48
Shakib Hossain <3992124+shakib609@users.noreply.github.com>
2022-11-01 01:35:15 +0600

    Resolve conflicts content/home/examples/a-simple-component.js

--- content/home/examples/a-simple-component.js
+++ content/home/examples/a-simple-component.js
@@ -1,22 +1,7 @@
 class HelloMessage extends React.Component {
   render() {
-<<<<<<< HEAD
-    return (
-      <div>
-        হ্যালো {this.props.name}
-      </div>
-    );
+    return <div>হ্যালো {this.props.name}</div>;
   }
 }
 
-ReactDOM.render(
-  <HelloMessage name="টেইলর" />,
-  document.getElementById('hello-example')
-);
-=======
-    return <div>Hello {this.props.name}</div>;
-  }
-}
-
-root.render(<HelloMessage name="Taylor" />);
->>>>>>> e21b37c8cc8b4e308015ea87659f13aa26bd6356
+root.render(<HelloMessage name="টেইলর" />);
